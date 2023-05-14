9e59d85a7a65a1b5359a5427829b8f9e2e0fd7e6
Chavindu13 <Chavindu13@DESKTOP-0GRPOM5>
2023-05-14 17:13:43 +0530

    Add project files.

---
+++ HighlightWord/HighlightWordTag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.Threading;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using System.Windows.Media;
+
+namespace HighlightWord
+{
+    internal class HighlightWordTag : TextMarkerTag
+    {
+        public HighlightWordTag() : base("MarkerFormatDefinition/HighlightWordFormatDefinition") { }
+    }
+
+    [Export(typeof(EditorFormatDefinition))]
+    [Name("MarkerFormatDefinition/HighlightWordFormatDefinition")]
+    [UserVisible(true)]
+    internal class HighlightWordFormatDefinition : MarkerFormatDefinition
+    {
+        public HighlightWordFormatDefinition()
+        {
+            this.BackgroundColor = Colors.Yellow;
+            this.ForegroundColor = Colors.Red;
+            this.DisplayName = "Highlight Word";
+            this.ZOrder = 5;
+        }
+    }
+}
